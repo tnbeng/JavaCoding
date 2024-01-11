70cd9c56ba1d1575c8f318cadd70abca92b89c7a
tnbeng <btarak398@gmail.com>
2024-01-11 08:49:22 +0530

    1.1

--- Java Coding Vs/Pattern Program/pattern1.java
+++ Java Coding Vs/Pattern Program/pattern1.java
@@ -11,6 +11,7 @@ public class pattern1 {
        }
        System.out.println();
     }
+
     System.out.println("Pattern 2");
     for(int i=0;i<5;i++)
     {
@@ -20,6 +21,8 @@ public class pattern1 {
        }
        System.out.println();
     }
+       System.out.println();
+    
 
     System.out.println("Pattern 3");
     for(int i=0;i<5;i++)
@@ -48,7 +51,7 @@ public class pattern1 {
          System.out.print(j+" ");  
        }
        System.out.println();
-    }
+    }  
     System.out.println("Pattern 5");
     for(int i=0;i<5;i++)
     {
@@ -60,11 +63,68 @@ public class pattern1 {
          }
          else
          {
-            System.out.print("  ");
+            System.out.print(" ");
          }
        }
        System.out.println();
     }
 
+    System.out.println("Pattern 6");
+        int count=1;
+        for(int i=1;i<=5;i++)
+        {
+           for(int j=1;j<=5;j++)
+           { 
+            if(count<10)
+            {
+               System.out.print("0");
+            }
+            System.out.print(count+++" ");
+           }
+           System.out.println();
+        }
+    System.out.println("Pattern 6");
+       
+        for(int i=1;i<=5;i++)
+        {
+           for(int j=1;j<=5;j++)
+           { 
+            if(i*j<10)
+            {
+               System.out.print("0");
+            }
+            System.out.print(i*j+" ");
+           }
+           System.out.println();
+        }
+   
+    System.out.println("Pattern 7");
+       for(int i=0;i<5;i++)
+       {
+         for(int k=0;k<5-i;k++)
+         {
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<=i;j++)
+         {
+            System.out.print("*");
+         }
+         System.out.println();
+       }
+    System.out.println("Pattern 8");
+       for(int i=0;i<5;i++)
+       {
+         for(int k=0;k<5-i;k++)
+         {
+            System.out.print(" ");
+         }
+
+         for(int j=0;j<=i;j++)
+         {
+            System.out.print("* ");
+         }
+         System.out.println();
+       }
    }
 }
